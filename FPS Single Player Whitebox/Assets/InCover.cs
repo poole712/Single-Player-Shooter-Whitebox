56c7211d895b67f3ce7e81d159970671910b4e87
Fletcher <fpoole666@gmail.com>
2020-08-21 11:42:45 +1200

    More LD progress

---
+++ FPS Single Player Whitebox/Assets/InCover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InCover : MonoBehaviour
+{
+    public static bool playerBehindCover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBehindCover = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("LowCover"))
+        {
+            playerBehindCover = true;
+        }
+        else
+        {
+            playerBehindCover = false;
+        }
+    }
+}
--- FPS Single Player Whitebox/Assets/Scripts/Dynamite.cs
+++ FPS Single Player Whitebox/Assets/Scripts/Dynamite.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dynamite : MonoBehaviour
 {
+    public Image dynamiteSprite;
     public bool hasDynamite;
     public GameObject wallToBlow;
     public AudioSource countDown;
     // Start is called before the first frame update
     void Start()
     {
+        dynamiteSprite.enabled = false;
         hasDynamite = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hasDynamite)
+        {
+            dynamiteSprite.enabled = true;
+        }
+        else
+        {
+            dynamiteSprite.enabled = false;
+        }
     }
     private void OnTriggerStay(Collider other)
     {
@@ -26,6 +36,7 @@ public class Dynamite : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.E) && hasDynamite && other.CompareTag("PlantZone"))
         {
+            hasDynamite = false;
             wallToBlow = other.gameObject;
             StartCoroutine(BlowUpWall());
         }
