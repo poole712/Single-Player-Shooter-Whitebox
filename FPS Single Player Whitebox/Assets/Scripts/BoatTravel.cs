1122e77a057e618f93276f4ea2ec98a40a593d78
Unknown <fpoole666@gmail.com>
2020-08-25 22:15:58 +1200

    BoatUpRiverWorking

---
+++ FPS Single Player Whitebox/Assets/Scripts/BoatTravel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatTravel : MonoBehaviour
+{
+    Animator boatMator;
+    public static bool OnTheBoat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnTheBoat = false;
+        boatMator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.boatMator.GetCurrentAnimatorStateInfo(0).IsName("AtDestination"))
+        {
+            OnTheBoat = false;
+        }
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            OnTheBoat = true;
+            boatMator.SetTrigger("PlayerOnBoard");
+        }
+    }
+}
